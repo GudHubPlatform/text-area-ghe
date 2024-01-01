a6cfba52b4ceeacab5c658e3c085d2ba8d1e4a1f
Kostyantyn Kosyuk <kostyantyn.kosyuk@gmail.com>
2024-01-01 10:13:40 +0200

    feature: added settings

--- src/data.js
+++ src/data.js
@@ -56,7 +56,62 @@ export default class GhStudyJournalData {
 				title: 'Options',
 				type: 'general_setting',
 				icon: 'menu',
-				columns_list: []
+				columns_list: [
+					[
+						{
+							type: 'ghElement',
+							property: 'data_model.app_id',
+							data_model: function () {
+								return {
+									data_type: 'app',
+									field_name: 'App',
+									name_space: 'app',
+									data_model: {
+										current_app: false,
+										interpretation: [
+											{
+												src: 'form',
+												id: 'with_text',
+												settings: {
+													editable: 1,
+													show_field_name: 1,
+													show_field: 1
+												}
+											}
+										]
+									}
+								};
+							}
+						},
+						{
+							type: 'ghElement',
+							property: 'data_model.data_field_id',
+							data_model: function (fieldModel) {
+								return {
+									data_type: 'field',
+									field_name: 'Data Field',
+									name_space: 'data_field',
+									data_model: {
+										app_id: fieldModel.data_model
+											.app_id
+									}
+								};
+							},
+							onInit: function (settingScope, fieldModel) {
+								settingScope.$watch(
+									function () {
+										return fieldModel.data_model
+											.app_id;
+									},
+									function (newValue) {
+										settingScope.field_model.data_model.app_id =
+											newValue;
+									}
+								);
+							}
+						}
+					]
+				]
 			}
 		];
 	}
